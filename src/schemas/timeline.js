0740e584ba8da9f75eb5b7c86a3816198adeb4a9
Tim Pietrusky <timpietrusky@gmail.com>
2019-05-11 18:54:36 +0200

    feat(animation): Create & update an animation
    Muation "setAnimation" to change an animation. Subscription "animationUpdated" to get notified when
    an animation was changed.

--- src/schemas/timeline.js
+++ src/schemas/timeline.js
@@ -2,9 +2,37 @@ import { gql } from 'apollo-server'
 import { PubSub } from 'graphql-subscriptions'
 export const pubsub = new PubSub()
 
+import { GraphQLScalarType } from 'graphql'
+import Kind from 'graphql/language'
+
+const ObjectScalarType = new GraphQLScalarType({
+  name: 'Object',
+  description: 'Arbitrary object',
+  parseValue: (value) => {
+    return typeof value === 'object' ? value
+      : typeof value === 'string' ? JSON.parse(value)
+      : null
+  },
+  serialize: (value) => {
+    return typeof value === 'object' ? value
+      : typeof value === 'string' ? JSON.parse(value)
+      : null
+  },
+  parseLiteral: (ast) => {
+    switch (ast.kind) {
+      case Kind.STRING: return JSON.parse(ast.value)
+      case Kind.OBJECT: throw new Error(`Not sure what to do with OBJECT for ObjectScalarType`)
+      default: return null
+    }
+  }
+})
+
 let _scenes = []
+let _animation = {}
 
 export const schema = gql`
+  scalar Object
+
   """
   Contains all scenes
   """
@@ -20,32 +48,61 @@ export const schema = gql`
     name: String
   }
 
+  type Animation {
+    """ID of the animation that is created by an external system (e.g. Thorium)"""
+    externalId: String
+    dimmer: Int
+    duration: Int 
+    color: [Int]
+    action: String
+    actionStrength: Float
+  }
+
+  input AnimationInput {
+    """ID of the animation that is created by an external system (e.g. Thorium)"""
+    externalId: String
+    dimmer: Int
+    duration: Int 
+    color: [Int]
+    action: String
+    actionStrength: Float
+  }
+
   type Query {
     getTimelineScenes: [Scene]
   }
 
   type Mutation {
     setTimelineScenes(scenes: [SceneInput]): [Scene]
+    setAnimation(animation: AnimationInput): Animation
   }
 
   type Subscription {
     timelineScenesUpdated: [Scene]
+    animationUpdated: Animation
   }
 `
 
 export const resolvers = {
-  Query: {
-    getTimelineScenes: () => _scenes
-  },
+  Object: ObjectScalarType,
+
+  Query: { getTimelineScenes: () => _scenes },
 
   Mutation: {
     setTimelineScenes: (_, { scenes }) => {
       _scenes = scenes
-
-      // Publish an event that the part was added
       pubsub.publish('scenesUpdated', _scenes)
+      console.log('setTimelineScenes', _scenes)
 
       return _scenes
+    },
+
+    setAnimation: (_, { animation }) => {
+      _animation = animation
+      pubsub.publish('animationUpdated', _animation)
+      console.log('setAnimation', _animation)
+
+      return _animation
     }
   },
 
@@ -53,6 +110,11 @@ export const resolvers = {
     timelineScenesUpdated: {
       resolve: payload => payload,
       subscribe: () => pubsub.asyncIterator('scenesUpdated')
+    },
+
+    animationUpdated: {
+      resolve: payload => payload,
+      subscribe: () => pubsub.asyncIterator('animationUpdated')
     }
   }
 }
